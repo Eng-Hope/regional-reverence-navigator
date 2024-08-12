dec00255db8d61051584901b2fcbdc13c4d016e9
Eng-Hope <hoperichardmaleko@gmail.com>
2024-08-12 11:20:34 +0300

    package2

--- src/app/_components/religion_search.tsx
+++ src/app/_components/religion_search.tsx
@@ -1,6 +1,8 @@
 "use server"
 import db from "@/Repository/db";
 import Religion from "../user/religion/_components/religion";
+import { religion } from "./user_religion_list";
+
 export default async function SearchReligion({name}:{name: String}) {
   const religions = await db.religion.findMany({
     where: {
@@ -16,8 +18,8 @@ export default async function SearchReligion({name}:{name: String}) {
     return (
       <section className="grid grid-cols-2 gap-[100px] p-[50px] mt-[80px]">
         {religions &&
-          religions.map((religion) => (
-            <Religion religion={religion}></Religion>
+          religions.map(({ religion }: { religion: religion }) => (
+            <Religion key={religion.id} religion={religion}></Religion>
           ))}
       </section>
     );
